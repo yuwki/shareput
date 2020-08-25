c369bc79dcb62656daa7ab1f003ac80e859a5562
yuwki <yuuki.ishii1208@gmail.com>
2020-08-25 21:17:17 +0900

    postsコントローラーの導入

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,18 @@
 class PostsController < ApplicationController
   def index
-    
+    @post = Post.new
+    @topic = Topic.find(params[:topic_id])
+    @posts = @topic.posts.includes(:user)
+  end
+
+  def create
+    @topic = Topic.find(params[:topic_id])
+    @post = @topic.posts.new(post_params)
+    @post.save
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:name, :text).merge(user_id: current_user.id)
   end
 end
